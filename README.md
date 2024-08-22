496e29f8dd23286766fa8633a5ad2a3b094edf80
Julian Andres Klode <julian.klode@canonical.com>
2024-08-22 11:25:18 +0200

    Initial commit, amd64

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ubuntu SBAT reset media
+
+Boot this image in secure boot mode to verify it, then
+disable secure boot and boot it again to clear SBAT revocations.
+
+The image automatically reboots into the firmware setup menu
+after 5 seconds in each case.
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+ARCH=amd64
+rm -rf ubuntu-mini-iso/$ARCH
+mkdir -p ubuntu-mini-iso/$ARCH/
+cp -a /usr/share/cd-boot-images-$ARCH/tree ubuntu-mini-iso/$ARCH/tree
+cp -a /usr/share/cd-boot-images-$ARCH/images ubuntu-mini-iso/$ARCH/images
+cat > ubuntu-mini-iso/$ARCH/tree/boot/grub/grub.cfg <<EOF
+if [ "\$shim_lock" = "y" ]; then
+    echo "This image verified correctly. Disable secure boot and boot it again to reset SBAT level"
+else
+    echo "The SBAT level has been reset."
+    echo "Please remove the media and re-enable secure boot before booting Ubuntu"
+fi
+echo
+echo "Entering the firmware menu in 5 seconds."
+sleep 1
+echo "Entering the firmware menu in 4 seconds."
+sleep 1
+echo "Entering the firmware menu in 3 seconds."
+sleep 1
+echo "Entering the firmware menu in 2 seconds."
+sleep 1
+echo "Entering the firmware menu in 1 seconds."
+sleep 1
+fwsetup
+EOF
+mkdir ubuntu-mini-iso/$ARCH/tree/.disk
+cat > ubuntu-mini-iso/$ARCH/tree/.disk/info <<EOF
+Ubuntu SBAT reset media
+EOF
+
+dest=../../sbat-reset-media-$ARCH.iso
+xorriso="$(cat /usr/share/cd-boot-images-amd64/xorriso-cmd.txt)"
+
+cd ubuntu-mini-iso/$ARCH
+$xorriso -o $dest
+cd ../..
+
+rm -rf ubuntu-mini-iso
+
